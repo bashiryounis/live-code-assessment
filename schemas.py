55f0dedf877b22a187da6a91c33c994d7940c62e
bashiryounis <ybashir344@gmail.com>
2025-10-03 23:15:30 +0300

    - schema defination

--- schemas.py
+++ schemas.py
@@ -1,2 +1,24 @@
-
+from pydantic import BaseModel
+from  datetime import datetime
 # TODO: Create the pydantic schemas for the endpoints (use models.py as reference)
+
+class Context(BaseModel): 
+    topic:str 
+    location:str 
+    language:str 
+
+class Tweet(BaseModel):
+    tweet:str 
+    context:Context 
+    author_name:str 
+    author_email:str 
+
+class TweetRequest(Context):
+    pass
+
+class TweetResponse(Tweet): 
+    id: int 
+    created_at:datetime 
+
+    class Config:
+        orm_mode = True
