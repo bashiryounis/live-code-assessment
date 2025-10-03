a2252ea634e832709c1d945ea6fd1978944cdf09
bashiryounis <ybashir344@gmail.com>
2025-10-03 23:14:53 +0300

    - setup celery broker and ap config

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
-
+load_dotenv(verbose=True)
 # TODO: Create the settings class to read environment variables
 
+class Settings(BaseSettings):
+    X_API_KEY : str = Field(env="X_API_KEY")
+    CELERY_BROKER_URL: str=Field(env="CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND: str=Field(env="CELERY_RESULT_BACKEND")
 
-
-settings = None
+settings = Settings()
